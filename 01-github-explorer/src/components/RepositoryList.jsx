6b02bde74e56bbe5490693d3fd7ca96938a6c39c
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-10-30 18:34:47 -0300

    listando repositórios

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -3,12 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 
 export function RepositoryList(){
 
@@ -26,10 +20,9 @@ export function RepositoryList(){
       <h1>List of repositories</h1>
 
       <ul>
-       <RepositoryItem repository={repository} />
-       <RepositoryItem repository={repository} />
-       <RepositoryItem repository={repository} />
-       <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
